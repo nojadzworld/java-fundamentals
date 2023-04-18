f713252c9fe0f7c20ca7a99bec130474bad004ce
DaJon Ewing <dajone37@gmail.com>
2023-04-17 18:07:42 -0700

    clock added

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 public static String pluralize(String word, int number) {
@@ -21,6 +23,8 @@ public static void main(String[] args) {
   System.out.println("I own " + storeCount + " " + pluralize("store", storeCount) + ".");
 
   flipNHeads(1);
+
+  clock();
 }
 
 public static void flipNHeads(int n) {
@@ -35,6 +39,20 @@ while (headsInARow < n) {
 
 }
 System.out.println("It took " + flips + " flips to flip " + n + " heads in a row.");
+}
+
+public static void clock() {
+  LocalDateTime prevTime = LocalDateTime.MIN;
+
+  while (true) {
+    LocalDateTime now = LocalDateTime.now();
+
+    if (now.getSecond() != prevTime.getSecond()) {
+      String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+      System.out.println(time);
+      prevTime = now;
+    }
+  }
 }
 
 }
